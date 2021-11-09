e83b496a99976a1a5d66ce85eb561dfba10bb74b
Adrián Bernardi <adrian.bernardi@gmail.com>
2021-11-09 12:02:03 -0300

    Fix pyflakes errors and coding style

--- invoice.py
+++ invoice.py
@@ -1,5 +1,6 @@
 # The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
@@ -9,11 +10,13 @@ from trytond.transaction import Transaction
 class CancelReason(ModelSQL, ModelView):
     'Invoice Cancel Reason'
     __name__ = 'account.invoice.cancel.reason'
+
     name = fields.Char('Name', translate=True)
 
 
 class Invoice(metaclass=PoolMeta):
     __name__ = 'account.invoice'
+
     cancel_reason = fields.Many2One('account.invoice.cancel.reason',
         'Cancel Reason', states={
             'required': ((Eval('state') == 'cancelled')
--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.account_invoice_cancel_reason.tests.test_account_invoice_cancel_reason import suite
+    from trytond.modules.account_invoice_cancel_reason.tests.test_account_invoice_cancel_reason \
+        import suite
 except ImportError:
     from .test_account_invoice_cancel_reason import suite
 
